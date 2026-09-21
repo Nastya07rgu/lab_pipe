5acd27929a758aaa54c542d1bb3404c235cca8f8
Анастасия Белова <word.2025@mail.ru>
2026-09-21 16:41:15 +0300

    каркас проекта

---
+++ lab_belova/lab_belova.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+struct Pipe {
+	string name;
+	float lenght;
+	int diametr;
+	bool status;
+	bool isAdded = false;
+};
+
+struct CS {
+	string name;
+	int number_work;
+	int number_work_online;
+	string class_cs;
+	bool isAdded = false;
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	return 0;
+
+}
