94012973bb6287ae526c68ca409057cb12b7282e
Анастасия Белова <word.2025@mail.ru>
2026-09-21 18:23:42 +0300

    добавлено меню и просмотр объектов

--- lab_belova/lab_belova.cpp
+++ lab_belova/lab_belova.cpp
@@ -60,6 +60,43 @@ string ReadNonEmptyString(const string& prompt) {
     }
 }
 
+void Show_menu() {
+    cout << "\n1. Добавить трубу" << endl;
+    cout << "2. Добавить КС" << endl;
+    cout << "3. Просмотр всех объектов" << endl;
+    cout << "4. Редактировать трубу" << endl;
+    cout << "5. Редактировать КС" << endl;
+    cout << "6. Сохранить" << endl;
+    cout << "7. Загрузить" << endl;
+    cout << "0. Выход" << endl;
+    cout << "Выберите действие: ";
+}
+
+void Show_All(const Pipe& pipe, const CS& cs) {
+    cout << "\n=== Все объекты ===" << endl;
+
+    if (pipe.isAdded) {
+        cout << "\n--- Труба ---" << endl;
+        cout << "Километровая отметка: " << pipe.name << endl;
+        cout << "Длина: " << pipe.length << " км" << endl;
+        cout << "Диаметр: " << pipe.diametr << " мм" << endl;
+        cout << "В ремонте: " << (pipe.status ? "да" : "нет") << endl;
+    }
+    else {
+        cout << "Труба не добавлена." << endl;
+    }
+
+    if (cs.isAdded) {
+        cout << "\n--- Компрессорная станция ---" << endl;
+        cout << "Название: " << cs.name << endl;
+        cout << "Всего цехов: " << cs.number_work << endl;
+        cout << "Цехов в работе: " << cs.number_work_online << endl;
+        cout << "Класс: " << cs.class_cs << endl;
+    }
+    else {
+        cout << "КС не добавлена." << endl;
+    }
+}
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
